8fbe2f46096022b90cdcc16ecb75148a5f4a4964
Rahul <rahulkashyap4019@gmail.com>
2022-11-08 09:38:05 +0530

    Make redux store persistent using persist-redux package

--- src/index.js
+++ src/index.js
@@ -3,20 +3,35 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { Toaster } from 'react-hot-toast';
 
 import rootReducer from './services/Reducers/index';
 import './styles/index.css';
 import App from './components/App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
+
 console.log('Before STATE', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
       <Toaster />
     </Provider>
   </React.StrictMode>
